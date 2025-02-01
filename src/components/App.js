da75e416990b83ee3bfa3ed7921fb608431582e0
K1ngHandy <bolds-slicks0g@icloud.com>
2025-02-01 12:13:12 -0500

    add change handler and submit handler

--- src/components/App.js
+++ src/components/App.js
@@ -16,11 +16,12 @@ export default function App() {
   const [friends, setFriends] = useState([]) // careful what you initialize your state to
 
   // 🔥 STEP 1 - WE NEED STATE TO HOLD ALL VALUES OF THE FORM!
-  const formValues = {} // fix this using the state hook
+  const [formValues, setFormValues] = useState([]) // fix this using the state hook
 
   const updateForm = (inputName, inputValue) => {
     // 🔥 STEP 8 - IMPLEMENT a "form state updater" which will be used inside the inputs' `onChange` handler
     //  It takes in the name of an input and its value, and updates `formValues`
+    setFormValues({ ...formValues, [inputName]: inputValue })
   }
 
   const submitForm = () => {
@@ -29,6 +30,22 @@ export default function App() {
     //  b) prevent further action if either username or email or role is empty string after trimming
     //  c) POST new friend to backend, and on success update the list of friends in state with the new friend from API
     //  d) also on success clear the form
+    const friend = { 
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+    if (!friend.username || !friend.email || !friend.role) {
+      return
+    }
+    axios.post('fakeapi.com/friends', friend)
+      .then(res => {
+        setFriends([ ...friends, res.data ])
+        setFormValues(initialFormValues)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   useEffect(() => {
@@ -43,7 +60,9 @@ export default function App() {
         // 🔥 STEP 2 - The form component needs its props.
         //  Check implementation of FriendForm
         //  to see what props it expects.
-        values={{}}
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
       />
 
       {
--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -9,12 +9,17 @@ export default function FriendForm(props) {
     // a) pull the name of the input from the event object
     // b) pull the value of the input from the event object
     // c) use the `update` callback coming in through props
+    const name = evt.target.name
+    const value = evt.target.value
+    update(name, value);
   }
 
   const onSubmit = evt => {
     // 🔥 STEP 7 - IMPLEMENT the submit handler
     // a) don't allow the browser to reload!
     // c) use the `submit` callback coming in through props
+    evt.preventDefault()
+    submit()
   }
 
   return (
@@ -23,15 +28,29 @@ export default function FriendForm(props) {
         {/* ////////// TEXT INPUTS ////////// */}
         {/* ////////// TEXT INPUTS ////////// */}
         {/* ////////// TEXT INPUTS ////////// */}
-        <label>Username
+        <label type='text'>Username
           {/* 🔥 STEP 3 - Make an input of type `text` for username.
               Controlled inputs need `value` and `onChange` props.
               Inputs render what they're told - their current value comes from app state.
               At each keystroke, a change handler fires to change app state. */}
+          <input 
+            onChange={onChange} 
+            name='username' 
+            type='text' 
+            placeholder='type a username...' 
+            value={values.username}
+          />
         </label>
 
-        <label>Email
+        <label type='email'>Email
           {/* 🔥 STEP 4 - Make an input of type `email` or `text` for email. */}
+          <input 
+            onChange={onChange} 
+            name='email' 
+            type='email' 
+            placeholder='type an email...' 
+            value={values.email}
+          />
         </label>
 
         {/* ////////// DROPDOWN ////////// */}
@@ -39,6 +58,12 @@ export default function FriendForm(props) {
         {/* ////////// DROPDOWN ////////// */}
         <label>Role
           {/* 🔥 STEP 5 - Make dropdown for role. */}
+          <select name='dropdown' onChange={onChange} type='dropdown'>
+            <option value=''>-- Select a Role --</option>
+            <option value='Friend'>Friend</option>
+            <option value='Family'>Family</option>
+            <option value='Colleague'>Colleague</option>
+          </select>
         </label>
 
         <div className='submit'>
